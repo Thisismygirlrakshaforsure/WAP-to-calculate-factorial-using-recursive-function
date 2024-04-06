73e20c37fb541f7dd5331e3808e3f5952c15be70
Raksha Adhikari <156924801+Thisismygirlrakshaforsure@users.noreply.github.com>
2024-04-06 08:55:09 +0545

    Create recursive.c

---
+++ recursive.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+    int fact (int);
+    void main()
+    {
+        int f;
+        printf("enter a no.");
+        scanf("%d", &n);
+        fact(i=1; i>=n; i++)
+        {
+        printf("\n %d", fact(i));
+        }
+        getch();
+    }
+        int fact (int i)
+        {
+            if (i=0)
+            {
+            return 1;
+            }
+            else
+            {
+            return fact (i)*fact (i-1);
+        }
+    }
